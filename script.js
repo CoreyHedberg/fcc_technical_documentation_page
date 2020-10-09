a4a5b4dd940e3f8c69d4aad0eedada15f4d390b2
Corey Hedberg <corey@coreyhedberg.dev>
2020-10-08 20:24:03 -0400

    Back at it. Got some more user stories completed.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 /*
     This website was written by Corey Hedberg for the purposes of the Free Code Camp Responsive Web Design Certification "Build a Technical Documentation Page" challenge.
+    corey@coreyhedberg.dev
+    https://coreyhedberg.dev
 */
